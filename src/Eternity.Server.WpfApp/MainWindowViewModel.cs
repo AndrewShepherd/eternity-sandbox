05fd687eb5bf3f3fa6b9e5b1f22105b2dd45d3a6
shepherd_a@hotmail.com <shepherd_a@hotmail.com>
2025-08-18 13:17:41 +1000

    Adding the Client and Server apps

---
+++ src/Eternity.Server.WpfApp/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eternity.Server.WpfApp
+{
+	public sealed class MainWindowViewModel()
+	{
+		public string TestString => "Eternity.Server.WpfApp";
+	}
+}
